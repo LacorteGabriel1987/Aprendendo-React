a87a5edadc74b431dfa6397eb0eff8a1f7721077
Gabriel Lacorte <gabrielslacorte@hotmail.com>
2022-10-10 16:07:00 -0300

    Iniciando projeto com React

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  const name = 'Gabriel'
+  function sum (a,b){
+    return a+b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+  return (
+    <div className="App">
+     <h1>Alterando JSX!</h1>
+     <p>Olá {name}</p>
+     <p>Soma: {sum(1,2)}</p>
+     <img src={url} alt="Minha imagem"/>
+     </div>
+  );
+}
+
+export default App;
